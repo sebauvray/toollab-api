6b8e39c8704a4dfe67cbc88f4f245b2934850113
Redha El hanti <relhanti@gmail.com>
2025-06-13 13:16:47 +0200

    fix: add classroom

--- app/Models/Classroom.php
+++ app/Models/Classroom.php
@@ -10,21 +10,40 @@ use Illuminate\Database\Eloquent\Relations\MorphToMany;
 class Classroom extends Model
 {
     use HasFactory;
-    
+
     protected $fillable = [
         'name',
         'years',
         'type',
         'size',
+        'school_id',
+        'cursus_id',
+        'level_id',
+        'gender'
     ];
 
-    public function roles(): morphToMany
+    public function roles(): MorphToMany
     {
         return $this->morphToMany(Role::class, 'roleable');
     }
-    
-    public function school(): belongsTo
+
+    public function school(): BelongsTo
     {
         return $this->belongsTo(School::class);
+    }
+
+    public function cursus(): BelongsTo
+    {
+        return $this->belongsTo(Cursus::class);
+    }
+
+    public function level(): BelongsTo
+    {
+        return $this->belongsTo(CursusLevel::class, 'level_id');
+    }
+
+    public function userRoles()
+    {
+        return $this->morphMany(UserRole::class, 'roleable');
     }
 }
