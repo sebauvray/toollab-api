d9058a411a844d02175da6e2a4b817a680a97170
Redha El hanti <relhanti@gmail.com>
2025-06-16 00:41:19 +0200

    config: database seeder simplified

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,57 +17,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            UserSeeder::class,
+/*            UserSeeder::class,*/
             RoleSeeder::class,
-            SchoolSeeder::class,
+/*            SchoolSeeder::class,*/
         ]);
-
-        //Factories
-        if (ENV('APP_ENV') !== 'production') {
-            User::factory()->count(9)->create();
-            School::factory()->count(5)->create();
-
-            $users = User::all();
-            $roles = Role::all();
-            $schools = School::all();
-            $families = Family::all();
-
-            $schools->each(function ($school) use ($schools) {
-                // Calculate a random number of classes for this school
-                $classCount = max(3, ceil(25 / $schools->count() + random_int(-2, 2)));
-                
-                Classroom::factory()
-                    ->count($classCount)
-                    ->forSchool($school)
-                    ->create();
-            });
-
-            //Get role responisble
-            $responsibleRole = $roles->firstWhere('name', 'Responsible');
-
-            if ($responsibleRole && $users->isNotEmpty() && $roles->isNotEmpty() && $schools->isNotEmpty()) {
-                foreach ($users as $user) {
-                    $randomRole   = $roles->random();
-                    $randomSchool = $schools->random();
-                     
-                    //Create family randomly
-                    if (rand(0, 1)) { 
-                        $family = Family::create();
-                        
-                        $user->roles()->create([
-                            'role_id' => $responsibleRole->id,
-                            'roleable_id' => $family->id,
-                            'roleable_type' => 'family'
-                        ]);
-                    }            
-                    // add randoom role
-                    $user->roles()->create([
-                        'role_id' => $randomRole->id,
-                        'roleable_id' => $randomSchool->id,
-                        'roleable_type' => 'school'
-                    ]);
-                }
-            }
-        }
    }
 }
