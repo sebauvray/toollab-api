1bf61e1950a16880ac08e4ac959f5fd178b22dbd
Auvray Sébastien <auvraysebastien75@gmail.com>
2026-07-06 13:41:13 +0200

    fix(emails): logo en PNG hébergé, le SVG inline ne s'affiche pas sur Gmail

---
+++ resources/views/emails/partials/logo.blade.php
@@ -0,0 +1,6 @@
+{{-- Logo Toollab pour les emails.
+     Image raster (PNG) hébergée dans public/images et servie en absolu :
+     les clients mail (Gmail, Outlook…) ne rendent pas le SVG inline. --}}
+<img src="{{ config('app.url') }}/images/logo-email.png"
+     alt="Toollab" width="276" height="52"
+     style="display:block; margin:0 auto; border:0; outline:none; text-decoration:none;">
